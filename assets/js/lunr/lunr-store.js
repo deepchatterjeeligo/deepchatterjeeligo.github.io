22a94dfb8eb0c76058b1a5f9abe6d97557e02a22
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-05-20 15:58:16 +0000

    Deploy deepchatterjeeligo/portfolio-project to github.com/deepchatterjeeligo/portfolio-project.git:gh-pages

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "Experience with Jekyll.",
+        "excerpt":"The days following my Ph.D. defense and dissertation submission was the perfect time for me to create a personal website, something I had been waiting to do for a long time. I was able to create a website in slightly over a weekend with Jekyll. Jekyll is an awesome tool...","categories": [],
+        "tags": [],
+        "url": "https://deepchatterjeeligo.github.io/portfolio-project//experience-with-jekyll/",
+        "teaser": null
+      }]
